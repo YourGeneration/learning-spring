5caa3ee823083941a2b392f4603a22a49e8264ba
YourGeneartion <beata.podgorska26@gmail.com>
2022-05-27 15:20:51 +0200

    RESTful

--- src/main/java/com/example/learningspring/business/ReservationService.java
+++ src/main/java/com/example/learningspring/business/ReservationService.java
@@ -82,4 +82,24 @@ public class ReservationService {
         });
         return guestList; 
     }
+
+    public List<Room> getRooms(){
+        Iterable<Room> rooms = this.roomRepository.findAll();
+        List<Room> roomList = new ArrayList<>();
+        rooms.forEach(room->{roomList.add(room);});
+        roomList.sort(new Comparator<Room>() {
+            @Override
+            public int compare(Room o1,Room o2){
+                return o1.getRoomNumber().compareTo(o2.getRoomNumber());
+            }
+        });
+        return roomList; 
+    }
+
+    public void addGuests(Guest guest) {
+        if(guest == null){
+            throw new RuntimeException("Guest Cannot be null");
+        }
+        this.guestRepository.save(guest);
+    }
 }
